d1c1ceacdca73c48be2e18bbb6d60c6af60a5d00
Kurochkin Mikhail <mkv-1724@mail.ru>
2021-10-05 01:03:46 +0300

    View Create In Progress

--- book_catalog/urls.py
+++ book_catalog/urls.py
@@ -20,4 +20,7 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('books/', views.BookListView.as_view(), name='book-list'),
+    path('book/<int:pk>', views.BookDetailView.as_view(), name='book-detail'),
+    path('author/<int:pk>', views.AuthorDetailView.as_view(), name='author-detail'),
 ]
--- book_catalog/views.py
+++ book_catalog/views.py
@@ -1,6 +1,21 @@
+from django.db import models
 from django.shortcuts import render
-
-# Create your views here.
+from django.views import generic
+from .models import Author, Book
 
 def index(request):
     return render(request, 'index.html')
+
+
+class BookListView(generic.ListView):
+    model = Book
+    template_name = 'book_list.html'
+
+
+class BookDetailView(generic.DetailView):
+    model = Book
+    template_name = 'book_detail.html'
+
+class AuthorDetailView(generic.DetailView):
+    model = Author
+    template_name = 'author_detail.html'
