a7010285790307c385346fabff62cf43714ad19b
Kurochkin Mikhail <mkv-1724@mail.ru>
2021-10-05 20:39:31 +0300

    initinally rest

--- django_library/urls.py
+++ django_library/urls.py
@@ -21,4 +21,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('catalog/', include('book_catalog.urls')),
     path('accounts/', include('django.contrib.auth.urls')),
+    path('api-auth/', include('rest_framework.urls')),
 ]
