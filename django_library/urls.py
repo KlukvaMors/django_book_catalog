59c1bb5b3f03bfc88ad490a68254525cb312579d
Kurochkin Mikhail <mkv-1724@mail.ru>
2021-10-05 17:42:19 +0300

    login page done

--- django_library/urls.py
+++ django_library/urls.py
@@ -20,4 +20,5 @@ from django.urls.conf import include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('catalog/', include('book_catalog.urls')),
+    path('accounts/', include('django.contrib.auth.urls')),
 ]
