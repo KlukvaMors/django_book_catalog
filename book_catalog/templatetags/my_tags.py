8a19ed63d49058ee2345250867374bb1ddcb658e
Kurochkin Mikhail <mkv-1724@mail.ru>
2021-10-08 16:16:57 +0300

    detail views updated

--- book_catalog/templatetags/my_tags.py
+++ book_catalog/templatetags/my_tags.py
@@ -5,3 +5,7 @@ register = template.Library()
 @register.filter
 def get_attr(obj, attr):
     return getattr(obj, attr)
+
+@register.filter
+def to_model_name(value):
+    return value.__class__.__name__.lower()
